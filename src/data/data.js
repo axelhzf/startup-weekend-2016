9a72649b354b0318bbd4731c2e9b54ba133851a3
Axel Hernández Ferrera <axelhzf@gmail.com>
2016-03-05 15:41:23 +0000

    initial commit

---
+++ src/data/data.js
@@ -0,0 +1,30 @@
+module.exports = {
+  celebrities: [
+    {
+      name: "Angelina",
+      coverPhoto: "http://....",
+      description: "",
+      diet: {
+        name: "Paleo",
+        description: "",
+        coverPhoto: "http://",
+        meals: {
+          breakfast: [
+            {
+              name: "",
+              coverPhoto: "",
+              recipe: "",
+
+            }
+          ],
+          lunch: [
+
+          ],
+          dinner: [
+
+          ]
+        }
+      }
+    }
+  ]
+};
