70e7e2ddb0787cac21458306e721647984441a7a
Axel Hernández Ferrera <axelhzf@gmail.com>
2016-03-06 15:02:53 +0000

    wip

--- gh-pages/app/js/app.js
+++ gh-pages/app/js/app.js
@@ -20288,9 +20288,9 @@ module.exports = _react2['default'].createClass({
 						null,
 						_react2['default'].createElement(PieChart, {
 							data: diet.nutritionalSummary,
-							width: 280,
-							height: 280,
-							radius: 70,
+							width: 290,
+							height: 290,
+							radius: 50,
 							innerRadius: 10,
 							sectorBorderColor: 'white'
 						})
